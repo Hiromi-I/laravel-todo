e4101f3b92f45c70ca4bc09fded70ef1e184cecf
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-11-04 11:06:27 +0900

    Add Status To Task Fillable

--- app/Task.php
+++ app/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'status'];
 
     public function getStatusLabelAttribute()
     {
