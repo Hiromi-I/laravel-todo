9aa6c7115acacc0e5d43a7ebca5be5b4788597c5
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-11-04 17:30:13 +0900

    Add Tadk Edit Styles

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,19 +2,34 @@
 @section('title', '編集')
 
 @section('content')
-  <form action="{{ action('TaskController@update', $task) }}" method="POST">
-    @csrf
-    @method('PATCH')
-    <input type="text" name="name" value="{{ old('name', $task->name) }}">
-    <select name="status" id="status">
-      @foreach($status_set as $status_index => $status_label)
-      <option value="{{ $status_index }}" {{ ($status_index == old('status', $task->status)) ? 'selected' : '' }}>{{ $status_label }}</option>
-      @endforeach
-    </select>
-    <input type="submit" value="Update">
-  </form>
+<h1 class="mb-4">タスク編集</h1>
 
   @error('name')
-    <p>{{ $message }}</p>
+  <div class="alert alert-danger mb-4" role="alert">{{ $message }}</div>
   @enderror
+
+  <form action="{{ action('TaskController@update', $task) }}" method="POST" class="taskForm">
+    @csrf
+    @method('PATCH')
+
+    <div class="form-group mb-4">
+      <label for="taskName">タスク名</label>
+      <input id="taskName" type="text" name="name" class="form-control" value="{{ old('name', $task->name) }}">
+    </div>
+
+    <div class="form-group mb-4">
+      <label for="status">進行状況</label>
+      <select id="status" name="status" class="custom-select custom-select-lg">
+        @foreach($status_set as $status_index => $status_label)
+        <option value="{{ $status_index }}" {{ ($status_index == old('status', $task->status)) ? 'selected' : '' }}>{{ $status_label }}</option>
+        @endforeach
+      </select>
+    </div>
+
+    <div class="buttonContainer">
+      <input type="submit" value="Update" class="btn btn-primary btn-lg">
+    </div>
+  </form>
+
+
 @endsection
