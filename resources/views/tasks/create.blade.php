1e4d7fac5f80964ddb9275ae213f3c4ca9d753f3
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-11-04 17:33:56 +0900

    Add Task Create Styles

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,13 +2,23 @@
 @section('title', '新規作成')
 
 @section('content')
-  <form action="{{ action('TaskController@store') }}" method="POST">
-    @csrf
-    <input type="text" name="name">
-    <input type="submit" value="Add">
-  </form>
+<h1 class="mb-4">タスクを新規作成</h1>
 
   @error('name')
-    <p>{{ $message }}</p>
+  <div class="alert alert-danger mb-4" role="alert">{{ $message }}</div>
   @enderror
+
+  <form action="{{ action('TaskController@store') }}" method="POST" class="taskForm">
+    @csrf
+
+    <div class="form-group mb-4">
+      <label for="taskName">タスク名</label>
+      <input id="taskName" type="text" name="name" class="form-control">
+    </div>
+
+    <div class="buttonContainer">
+      <input type="submit" value="Add" class="btn btn-primary btn-lg">
+    </div>
+  </form>
+
 @endsection
